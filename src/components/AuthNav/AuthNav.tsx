ed299752c2e67c2ae702dafd8b029d02e963990e
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-04-10 23:12:16 +0300

    v. 1.3. Create component AuthNav

---
+++ src/components/AuthNav/AuthNav.tsx
@@ -0,0 +1,71 @@
+import { FiLogOut } from "react-icons/fi";
+import clsx from "clsx";
+
+interface AuthNavProps {
+  onAction: (mode?: "login" | "register") => void;
+  variant?: "menu" | "header";
+}
+
+const headerLoginLogoutBtn =
+  "flex gap-2 item-center text-base/tight font-bold cursor-pointer hover:text:grey transition-all duration-300 ease-in";
+const headerRegisterBtn =
+  "bg-black dark:bg-dark-light rounded-xl px-10 py-3.5 text-white cursor-pointer hover:bg-grey transition-all duration-300 ease-in";
+const menuUniversalBtn =
+  "bg-accent text-black flex gap-2 justify-center items-center text-base font-bold cursor-pointer border-2 border-accent box-border rounded-xl px-10 py-3.5 hover:bg-accent-light transition-all duration-300 ease-in";
+
+const AuthNav: React.FC<AuthNavProps> = ({ onAction, variant = "header" }) => {
+  const handleLogout = () => {
+    onAction();
+  };
+
+  return (
+    <>
+      <button
+        onClick={handleLogout}
+        className={
+          variant === "header" ? headerLoginLogoutBtn : menuUniversalBtn
+        }
+      >
+        <FiLogOut
+          size={20}
+          color={variant === "header" ? "var(--color-accent)" : "black"}
+        />
+        Logout
+      </button>
+
+      <div
+        className={
+          variant === "header" ? "flex gap-4" : "flex gap-4 w-full flex-col"
+        }
+      >
+        <button
+          onClick={() => onAction("login")}
+          className={
+            variant === "header" ? headerLoginLogoutBtn : menuUniversalBtn
+          }
+        >
+          <svg
+            className={clsx(
+              "w-5 h-5 fill-none",
+              variant === "header" ? "stroke-accent" : "stroke-black"
+            )}
+          >
+            <use href={`/sprite.svg#icon-log-in`} />
+          </svg>
+          Log in
+        </button>
+
+        <button
+          onClick={() => onAction("register")}
+          className={
+            variant === "header" ? headerRegisterBtn : menuUniversalBtn
+          }
+        >
+          Registration
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default AuthNav;
