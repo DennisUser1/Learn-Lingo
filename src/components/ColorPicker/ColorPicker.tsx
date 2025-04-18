c06e6d945d3c3717629b2d7e200a8b0117b8b61e
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-04-18 23:46:00 +0300

    v.1.10. Added hook useThemeColors

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { colors } from "shared/helpers/colors";
+import { useThemeColors } from "shared/hooks/useThemeColors";
 
 const ColorPicker: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const {selectedColor, changeTheme} = useThemeColors();
 
   const pickerRef = useRef<HTMLDivElement | null>(null);
 
@@ -20,15 +22,22 @@ const ColorPicker: React.FC = () => {
     return () => document.removeEventListener("click", handleClickOutside);
   }, []);
 
-  const handleColorSelect = () => {
+  const handleColorSelect = (color: {
+    main: string;
+    light: string;
+    gradientFrom: string;
+    gradientTo: string;
+  }) => {
+    changeTheme(color);
     setIsOpen(false);
   };
 
   return (
-    <div className="absolute to-6 right-6 color-picker" ref={pickerRef}>
+    <div className="absolute top-6 right-6 color-picker" ref={pickerRef}>
       <div
         className="w-8 h-8 rounded-full cursor-pointer border border-grey-light hover:scale-110 transition-all duration-300 ease-in"
         onClick={() => setIsOpen(!isOpen)}
+        style={{ backgroundColor: `var(${selectedColor})` }}
       ></div>
       <AnimatePresence>
         {isOpen && (
@@ -44,7 +53,7 @@ const ColorPicker: React.FC = () => {
                 key={color.main}
                 className="w-8 h-8 rounded-full cursor-pointer hover:scale-110 border border-grey-light transition-all duration-300 ease-in"
                 style={{ backgroundColor: `var(${color.main})` }}
-                onClick={() => handleColorSelect()}
+                onClick={() => handleColorSelect(color)}
               ></li>
             ))}
           </motion.ul>
