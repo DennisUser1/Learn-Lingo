45a93c31ad749114a2922a5707b1f7636c08515d
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-04-24 23:36:08 +0300

    v.1.14. Create component TeacherAvatar.tsx

--- src/components/TeacherCard/TeacherCard.tsx
+++ src/components/TeacherCard/TeacherCard.tsx
@@ -1,3 +1,4 @@
+import TeacherAvatar from "components/TeacherAvatar/TeacherAvatar";
 import { Teacher } from "shared/types/Teacher";
 
 const TeacherCard: React.FC<{ teacher: Teacher }> = ({ teacher }) => {
@@ -6,7 +7,9 @@ const TeacherCard: React.FC<{ teacher: Teacher }> = ({ teacher }) => {
     <li
       data-aos="fade-up"
       className="bg-white dark:bg-dark-light flex flex-col desktop:flex-row gap-12 p-6 rounded-3xl relative"
-    ></li>
+    >
+        <TeacherAvatar  teacher={teacher} />
+    </li>
   );
 };
 
