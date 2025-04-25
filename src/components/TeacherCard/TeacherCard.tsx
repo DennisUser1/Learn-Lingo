3f068b59a1598f6c804cf86bee09669d80643092
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-04-25 22:59:43 +0300

    v.1.15. Create component ToggleFavorite.tsx

--- src/components/TeacherCard/TeacherCard.tsx
+++ src/components/TeacherCard/TeacherCard.tsx
@@ -1,14 +1,15 @@
 import TeacherAvatar from "components/TeacherAvatar/TeacherAvatar";
+import ToggleFavorite from "components/ToggleFavorite/ToggleFavorite";
 import { Teacher } from "shared/types/Teacher";
 
 const TeacherCard: React.FC<{ teacher: Teacher }> = ({ teacher }) => {
-
   return (
     <li
       data-aos="fade-up"
       className="bg-white dark:bg-dark-light flex flex-col desktop:flex-row gap-12 p-6 rounded-3xl relative"
     >
-        <TeacherAvatar  teacher={teacher} />
+      <TeacherAvatar teacher={teacher} />
+      <ToggleFavorite teacher={teacher} />
     </li>
   );
 };
---
+++ src/components/ToggleFavorite/ToggleFavorite.tsx
@@ -0,0 +1,32 @@
+import { Teacher } from "shared/types/Teacher";
+
+interface ToggleFavoriteProps {
+  teacher: Teacher;
+  toggleFavorite: (id: string) => void;
+  isFavorite: boolean;
+}
+
+const ToggleFavorite: React.FC<ToggleFavoriteProps> = ({
+  teacher,
+  toggleFavorite,
+  isFavorite,
+}) => {
+  return (
+    <button
+      onClick={() => toggleFavorite(teacher.id)}
+      className="absolute top-6 right-6 w-6.5 h-6.5 cursor-pointer transition-all ease-in group"
+    >
+      {isFavorite ? (
+        <svg className="w-full h-full hover:scale-110 transition-all duration-200 ease-in">
+          <use href={`/sprite.svg#icon-heart-yellow`} />
+        </svg>
+      ) : (
+        <svg className="w-full h-full stroke-black dark:stroke-white fill-none hover:stroke-yellow-300 hover:scale-110 transition-all duration-200 ease-in">
+          <use href={`/sprite.svg#icon-heart`} />
+        </svg>
+      )}
+    </button>
+  );
+};
+
+export default ToggleFavorite;
