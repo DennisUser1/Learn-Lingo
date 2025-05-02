849754400bd07d79a7aed8ab75d3d70e03b23bbd
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-05-02 23:56:43 +0300

    v.1.21. Added page FavoritesPage

--- src/pages/Favorites/FavoritesPage.tsx
+++ src/pages/Favorites/FavoritesPage.tsx
@@ -3,8 +3,12 @@
 const FavoritesPage: React.FC = () => { 
 
     return (
-        <main>
-            
+        <main className="container m-auto flex flex-col pb-4 pt-4 px-4 tablet:pb-8 tablet:px-8 desktop:px-32">
+            <ul className="flex flex-col gap-8 mb-16">
+                {favoriteTeachers.map((teacher) => (
+                    <TeacherCard key={teacher.id} teacher={teacher} />
+                ))}
+            </ul> 
         </main>
     )
 }
